2af896cfdcf7f3f521da89d673973783c3cbf989
João Jerónimo <joaopintojeronimo@gmail.com>
2014-07-31 14:34:22 +0100

    flatten arguments of STDEVS, add tests for it

--- public/tests.js
+++ public/tests.js
@@ -1284,6 +1284,12 @@
         {"call": "STDEVP([1,2],[3,4],[true,false])", "result": 1.118033988749895},
         {"call": "STDEVP([[1,2],[3,4],[true,false]])", "result": 1.118033988749895},
       ]},
+      {"function": "STDEVS", "tests": [
+        {"call": "STDEVS(1,2,3,4,true,false)", "result": 1.2909944487358056},
+        {"call": "STDEVS([1,2],[3,4],[true,false])", "result": 1.2909944487358056},
+        {"call": "STDEVS([[1,2],[3,4],[true,false]])", "result": 1.2909944487358056},
+        {"call": "STDEVS([[1,2],[3,4]])", "result": 1.2909944487358056},
+      ]},
       {"function": "MD5", "tests": [
         {"call": "MD5('jim@sutoiku.com')", "result": '3d508d960d5f63a9d9384baf6b4f67c3'},
         {"call": "MD5('ismael@sutoiku.com')", "result": 'a33dab7034e1fced4adb648f46eabe5d'}
