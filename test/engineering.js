4215ec9f8d5126d0c606d77122087d2d565d2670
Miguel Serrano <miguelvps@gmail.com>
2014-08-05 14:20:52 +0100

    add bin2dec function

--- test/engineering.js
+++ test/engineering.js
@@ -2,25 +2,33 @@ var error = require('../lib/error');
 var engineering = require('../lib/engineering');
 var should = require('should');
 
-// TODO
-test('BESSELI', function() {
-  should.equal(engineering.BESSELI(), undefined);
-});
+suite('Engineering', function() {
+  // TODO
+  test('BESSELI', function() {
+    should.equal(engineering.BESSELI(), undefined);
+  });
 
 
-// TODO
-test('BESSELJ', function() {
-  should.equal(engineering.BESSELI(), undefined);
-});
+  // TODO
+  test('BESSELJ', function() {
+    should.equal(engineering.BESSELJ(), undefined);
+  });
 
 
-// TODO
-test('BESSELK', function() {
-  should.equal(engineering.BESSELI(), undefined);
-});
+  // TODO
+  test('BESSELK', function() {
+    should.equal(engineering.BESSELK(), undefined);
+  });
 
 
-// TODO
-test('BESSELY', function() {
-  should.equal(engineering.BESSELI(), undefined);
+  // TODO
+  test('BESSELY', function() {
+    should.equal(engineering.BESSELY(), undefined);
+  });
+
+  test('BIN2DEC', function() {
+    engineering.BIN2DEC(1100100).should.equal(100);
+    engineering.BIN2DEC(1111111111).should.equal(-1);
+    engineering.BIN2DEC('a').should.equal(error.num);
+  });
 });
