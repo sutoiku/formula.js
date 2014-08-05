b24d5388c937c55061a4ccacfa0cf1eeeba830b3
Miguel Serrano <miguelvps@gmail.com>
2014-08-05 14:09:39 +0100

    add bessel functions

---
+++ test/engineering.js
@@ -0,0 +1,26 @@
+var error = require('../lib/error');
+var engineering = require('../lib/engineering');
+var should = require('should');
+
+// TODO
+test('BESSELI', function() {
+  should.equal(engineering.BESSELI(), undefined);
+});
+
+
+// TODO
+test('BESSELJ', function() {
+  should.equal(engineering.BESSELI(), undefined);
+});
+
+
+// TODO
+test('BESSELK', function() {
+  should.equal(engineering.BESSELI(), undefined);
+});
+
+
+// TODO
+test('BESSELY', function() {
+  should.equal(engineering.BESSELI(), undefined);
+});
