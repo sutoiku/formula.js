ddb4555a8c469840bb30fbe29ad613a0099e2cbb
Jim Alateras <jima@comware.com.au>
2014-09-12 07:26:02 +1000

    Removed reference to blueimp-md5

--- test/miscellaneous.js
+++ test/miscellaneous.js
@@ -3,11 +3,6 @@ var misc = require('../lib/miscellaneous');
 var should = require('should');
 
 suite('Miscellaneous', function() {
-  test('MD5', function() {
-    misc.MD5('jim@sutoiku.com').should.equal("3d508d960d5f63a9d9384baf6b4f67c3");
-    misc.MD5('ismael@sutoiku.com').should.equal("a33dab7034e1fced4adb648f46eabe5d");
-  });
-
   test('NUMERAL', function() {
     misc.NUMERAL(10000, '0,0.0000').should.equal("10,000.0000");
     misc.NUMERAL(10000.23, '0,0').should.equal("10,000");
