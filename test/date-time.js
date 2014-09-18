ef7e6bfb69776f8cc3821f268dde608e159e97fb
jesusjzp <jesusjzp@gmail.com>
2014-09-18 12:05:30 -0700

    Fixed NETWORKDAYS.INTL and modified relative test.

--- test/date-time.js
+++ test/date-time.js
@@ -100,18 +100,17 @@ suite('Date & Time', function() {
   });
 
   test('NETWORKDAYS', function() {
-    dateTime.NETWORKDAYS('1/1/1900', '1/2/1900').should.equal(1);
-    dateTime.NETWORKDAYS('1/1/1900', '2/1/1900').should.equal(23);
-    dateTime.NETWORKDAYS('1/1/1900', '2/1/1900', '1/2/1900').should.equal(22);
-    dateTime.NETWORKDAYS('1/1/1900', '2/1/1900', ['1/2/1900', '1/3/1900']).should.equal(21);
+    dateTime.NETWORKDAYS('12/4/2013', '12/4/2013').should.equal(1);
+    dateTime.NETWORKDAYS('12/4/2013', '1/4/2014', '1/1/2014').should.equal(22);
+    dateTime.NETWORKDAYS('12/4/2013', '1/4/2014', ['1/1/2014', '1/2/2014', '1/3/2014']).should.equal(20);
     dateTime.NETWORKDAYS('a', '1/2/1900').should.equal(error.value);
     dateTime.NETWORKDAYS('1/1/1900', 'a').should.equal(error.value);
     dateTime.NETWORKDAYS('1/1/1900', '2/1/1900', 'a').should.equal(error.value);
   });
 
   test('NETWORKDAYS.INTL', function() {
-    dateTime.NETWORKDAYS.INTL('1/1/1900', '1/2/1900').should.equal(1);
-    dateTime.NETWORKDAYS.INTL('1/1/1900', '1/2/1900', 2).should.equal(0);
+    dateTime.NETWORKDAYS.INTL('1/1/1900', '1/2/1900').should.equal(2);
+    dateTime.NETWORKDAYS.INTL('1/1/1900', '1/2/1900', 2).should.equal(1);
     dateTime.NETWORKDAYS.INTL('1/1/1900', '1/2/1900', -1).should.equal(error.value);
   });
 
@@ -196,9 +195,13 @@ suite('Date & Time', function() {
   });
 
   test('NETWORKDAYS', function() {
-    dateTime.NETWORKDAYS('2013-12-04', '2013-12-04').should.equal(0);
-    dateTime.NETWORKDAYS('2013-12-04', '2013-12-05').should.equal(1);
+    dateTime.NETWORKDAYS('2013-12-04', '2013-12-04').should.equal(1);
+    dateTime.NETWORKDAYS('2013-12-04', '2013-12-05').should.equal(2);
+    dateTime.NETWORKDAYS('2013-12-04', '2013-12-06').should.equal(3);
     dateTime.NETWORKDAYS('2013-12-04', '2013-12-07').should.equal(3);
-    dateTime.NETWORKDAYS('2013-12-04', '2013-12-09').should.equal(3);
+    dateTime.NETWORKDAYS('2013-12-04', '2013-12-08').should.equal(3);
+    dateTime.NETWORKDAYS('2013-12-04', '2013-12-09').should.equal(4);
+    dateTime.NETWORKDAYS('2013-12-07', '2013-12-07').should.equal(0);
+    dateTime.NETWORKDAYS('2013-12-07', '2013-12-08').should.equal(0);
   });
 });
