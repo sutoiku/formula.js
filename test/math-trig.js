d8775195dffeaca6f1da06d9fbaaa31a2b5c391c
Andrei Scheinkman <andreischeinkman@gmail.com>
2019-01-17 14:59:05 -0500

    Add support for sum_range argument and equality criteria in SUMIF

--- test/math-trig.js
+++ test/math-trig.js
@@ -700,6 +700,9 @@ suite('Math & Trig', function() {
 
   test("SUMIF", function() {
     mathTrig.SUMIF([1, 2, 3], '>2').should.equal(3);
+    mathTrig.SUMIF([1, 2, 3], '=2').should.equal(2);
+    mathTrig.SUMIF([1, 2, 3], '>2', [4, 5, 6]).should.equal(6);
+    mathTrig.SUMIF(['"A"', '"B"', '"B"'], '"B"', [1, 2, 3]).should.equal(5);
     mathTrig.SUMIF([
       [1, 1],
       [2, 2],
